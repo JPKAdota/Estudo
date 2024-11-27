649a3855f781823ababf249dd1eb4751d182e0ca
JPKAdota <joaokadota@gmail.com>
2024-11-27 18:20:43 -0300

    first commit

---
+++ exercicio3.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+num = int(input("Digite um número: "))
+num2 = int(input("Digite mais um número: "))
+
+soma = num+num2
+
+print(f"A soma dos números é: {soma}")
